54e9af5ba1451436694b03abd4bb6786e94b03cf
DMP <149814159+idkhate@users.noreply.github.com>
2024-12-06 16:36:57 -0700

    Add files via upload

---
+++ Black pink/script.js
@@ -0,0 +1,5 @@
+// Mostrar alerta en el formulario de contacto
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("¡Gracias por contactarnos! Te responderemos pronto.");
+});
